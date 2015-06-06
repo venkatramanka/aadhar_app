6cfb3ed3d61caea38c83e5df67c461ccabdfc4a1
Ashwin <s.ashwinkumar2490@gmail.com>
2015-06-06 11:38:35 +0530

    css js copied and sample home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  def login
+  	redirect_to root_path
+  end
+
+  def signup
+  	redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,9 @@ AadharApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
+  match '/login' => 'application#login', :as => "login"
+  match '/singup' => 'application#signup', :as => "registration"
 
   # See how all your routes lay out with "rake routes"
 
