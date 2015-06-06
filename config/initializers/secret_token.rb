9fba860852db1968c247c0b2149fbbaf5b73c083
venkatramanka <venkatka@outlook.com>
2015-06-06 10:16:59 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AadharApp::Application.config.secret_token = 'd47fc3df82ee532fb6d194042ac614d54225bcafbeecb7ade9905312843a9eb18b55e7be091cebc079c37e3d616d1cac26958fde8384221f8619b4f76e8c1ad0'
